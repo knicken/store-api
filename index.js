294514e282dfe3f903ee522c1ab938610bafce43
Benedict Gish <benedict.gish@sap.com>
2022-04-04 16:30:04 +0200

    CORS header

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
 const swaggerJson = require('./swagger.json')
 const routes = require('./routes');
 
 const PORT = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerJson));
 
