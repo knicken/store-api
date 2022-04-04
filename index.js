8453f3ce7dc01e9890a26094b3f323c2f6ed57e0
Benedict Gish <benedict.gish@sap.com>
2022-04-04 15:03:14 +0200

    Routes for products

--- index.js
+++ index.js
@@ -1,26 +1,12 @@
 const express = require('express');
 const app = express();
 const swaggerUi = require('swagger-ui-express');
-const swaggerJsdoc = require('swagger-jsdoc');
-
+const swaggerJson = require('./swagger.json')
 const routes = require('./routes');
 
 const PORT = process.env.PORT || 3000;
 
-const options = {
-    definition: {
-        openapi: '3.0.0',
-        info: {
-            title: 'Hello World',
-            version: '1.0.0',
-        },
-    },
-    apis: ['./routes.js'],
-};
-
-const swaggerSpec = swaggerJsdoc(options);
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerJson));
 
 routes.setup(app);
 app.listen(PORT)
--- routes.js
+++ routes.js
@@ -1,14 +1,48 @@
+const products = require('./products.json');
+const prices = require('./price.json')
+
 module.exports.setup = (app) => {
-    /**
-     * @swagger
-     * /:
-     *   get:
-     *     description: Returns the homepage
-     *     responses:
-     *       200:
-     *         description: hello world
-     */
+
     app.get('/', (req, res) => {
-        res.send('Hello World!');
+        res.redirect('/api-docs');
     });
+
+    app.get('/products', (req, res) => {
+        res.send(products)
+    })
+
+    app.get('/products/:id', (req, res) => {
+        const product = products.find(product => product.id == req.params.id)
+        if (product) {
+            res.send(product);
+        } else {
+            res.sendStatus('404');
+        }
+    })
+
+    app.get('/price/:id', (req, res) => {
+        const price = prices.find(price => price.id == req.params.id);
+        if (price) {
+            res.send(price);
+        } else {
+            res.sendStatus('404');
+        }
+    })
+
+    //TODO
+    app.post('/add-to-cart', (req, res) => {
+        res.sendStatus('501');
+    })
+
+    app.get('/cart/:id', (req, res) => {
+        res.sendStatus('501');
+    })
+
+    app.post('/login', (req, res) => {
+        res.sendStatus('501');
+    })
+
+    app.get('/user', (req, res) => {
+        res.sendStatus('501');
+    })
 };
